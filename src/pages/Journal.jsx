661cce1ea38d60391673191a676d0db352632be3
An Go <gorbunoochek@gmail.com>
2024-02-11 05:52:14 +0100

    small css update

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from "react"
 import TextareaAutosize from "react-textarea-autosize"
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { PlusCircleIcon } from "@heroicons/react/24/outline"
+import { SparklesIcon } from "@heroicons/react/24/outline"
 
 function Journal() {
   const [inputText, setInputText] = useState("")
+  const [AIResponse, setAIResponse] = useState("")
+
   const handleInputChange = (e) => {
     setInputText(e.target.value)
   }
@@ -13,30 +16,33 @@ function Journal() {
     setInputText("")
   }
   return (
-    <div className="flex flex-col h-screen box-border bg-blue-900">
-      <div className="w-full sticky">
+    <div className="flex flex-col h-screen box-border bg-blue-400">
+      <div className="flex flex-col h-full overflow-hidden flex-1 max-w-3xl w-full mx-auto px-5 m-auto  bg-white rounded text-sky-950 font-roboto">
         <form
           onSubmit={handleFormSubmit}
-          className="flex w-full justify-end items-center sticky border rounded-full mb-6 -mt-3 shadow-lg bg-white p-1 overflow-hidden"
+          className="flex flex-col w-full h-3/4 justify-end items-start sticky mb-6 -mt-3  p-1 overflow-hidden"
         >
-          <TextareaAutosize
-            autoFocus
-            className="flex-1 px-6 h-16 resize-none outline-none border-0 rounded bg-transparent text-sky-950 font-roboto"
-            name="chat"
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            placeholder="Write your message..."
-          />
-
           <button
             type="submit"
             className="h-16 w-16 hover:bg-blue-100 rounded-full flex items-center justify-center "
           >
             {" "}
-            <PaperAirplaneIcon className="h-7 w-7 text-blue-800" />
+            <PlusCircleIcon className="h-7 w-7 text-blue-800" />
           </button>
+          <TextareaAutosize
+            autoFocus
+            className="flex-1 w-full px-6 h-16 resize-none outline-none border-0 rounded bg-transparent text-sky-950 font-roboto"
+            name="chat"
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="How are you feeling today?"
+          />
         </form>
+        <div>
+          <SparklesIcon className="h-7 w-7 text-blue-800" />
+          <div> {AIResponse} </div>
+        </div>
       </div>
     </div>
   )
