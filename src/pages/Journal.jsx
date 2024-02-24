eca9a8a3bdfa1a55fa80b10b143f80a51fcec22a
An Go <gorbunoochek@gmail.com>
2024-02-24 15:11:49 +0100

    composant

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -1,44 +1,33 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, useContext } from "react"
 import TextareaAutosize from "react-textarea-autosize"
 import { PlusCircleIcon } from "@heroicons/react/24/outline"
 import { SparklesIcon } from "@heroicons/react/24/outline"
+import CreateNote from "../components/CreateNote"
+import { AuthContext } from "../context/auth.context"
 
 function Journal() {
-  const [inputText, setInputText] = useState("")
+  const { token, checkLogin } = useContext(AuthContext)
+
+  const [text, setText] = useState("")
   const [AIResponse, setAIResponse] = useState("")
 
   const handleInputChange = (e) => {
-    setInputText(e.target.value)
+    setText(e.target.value)
   }
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
-    setInputText("")
+    setText("")
   }
+
+  useEffect(() => {
+    checkLogin()
+  }, []) // faut redirect sur la page de login?
+
   return (
     <div className="flex flex-col h-screen box-border bg-blue-400">
       <div className="flex flex-col h-full overflow-hidden flex-1 max-w-3xl w-full mx-auto px-5 m-auto  bg-white rounded text-sky-950 font-roboto">
-        <form
-          onSubmit={handleFormSubmit}
-          className="flex flex-col w-full h-3/4 justify-end items-start sticky mb-6 -mt-3  p-1 overflow-hidden"
-        >
-          <button
-            type="submit"
-            className="h-16 w-16 hover:bg-blue-100 rounded-full flex items-center justify-center "
-          >
-            {" "}
-            <PlusCircleIcon className="h-7 w-7 text-blue-800" />
-          </button>
-          <TextareaAutosize
-            autoFocus
-            className="flex-1 w-full px-6 h-16 resize-none outline-none border-0 rounded bg-transparent text-sky-950 font-roboto"
-            name="chat"
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            placeholder="How are you feeling today?"
-          />
-        </form>
+        <CreateNote></CreateNote>
         <div>
           <SparklesIcon className="h-7 w-7 text-blue-800" />
           <div> {AIResponse} </div>
