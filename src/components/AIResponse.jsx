e2cafefba4b8cd6cfaffd11fff29c724d8f15313
An Go <gorbunoochek@gmail.com>
2024-02-27 18:20:03 +0100

    aiResp

--- src/components/AIResponse.jsx
+++ src/components/AIResponse.jsx
@@ -1,7 +1,36 @@
-import React from "react"
+import React, { useEffect, useState, useContext } from "react"
+import { AuthContext } from "../context/auth.context"
 
-function AIResponse(props) {
-  return <div></div>
+function AIResponse({ noteId }) {
+  const { getAIResponse, token } = useContext(AuthContext)
+  const [aiResponse, setAIResponse] = useState(null)
+
+  useEffect(() => {
+    const fetchAIResponse = async () => {
+      if (token && noteId) {
+        try {
+          const response = await getAIResponse(token, noteId)
+          setAIResponse(response)
+        } catch (error) {
+          console.log("Error fetching AI response:", error)
+        }
+      }
+    }
+
+    fetchAIResponse()
+  }, [token, noteId])
+
+  return (
+    <div>
+      {aiResponse ? (
+        <div>
+          <p>{aiResponse.text}</p>
+        </div>
+      ) : (
+        <p>No AI response found.</p>
+      )}
+    </div>
+  )
 }
 
 export default AIResponse
